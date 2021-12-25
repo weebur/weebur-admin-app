3c712084755b6afa28b0519817d9739b938aa071
elliot <elliot@wired.company>
2021-12-25 10:43:59 +0900

    build test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import { useEffect } from 'react';
-import api from '../api';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
