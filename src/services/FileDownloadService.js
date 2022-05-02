2de870302ac2b4b715323bc69d3e94a97fa98810
elliot <elliot@wired.company>
2022-05-02 12:05:50 +0900

    download logic

--- src/services/FileDownloadService.js
+++ src/services/FileDownloadService.js
@@ -4,7 +4,26 @@ import { message } from 'antd';
 export const download = async (fetcher, filename) => {
     try {
         const data = await fetcher();
-        fileDownload(data, `${filename}.csv`);
+
+        const blob = new Blob([data], { type: 'text/csv;charset=utf-8;' });
+        if (navigator.msSaveBlob) {
+            // IE 10+
+            navigator.msSaveBlob(blob, filename);
+        } else {
+            const link = document.createElement('a');
+            if (link.download !== undefined) {
+                // Browsers that support HTML5 download attribute
+                const url = URL.createObjectURL(blob);
+                link.setAttribute('href', url);
+                link.setAttribute('download', filename);
+                link.style.visibility = 'hidden';
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+            }
+        }
+
+        // fileDownload(data, `${filename}.csv`);
     } catch (e) {
         message.error(JSON.stringify(e));
     }
