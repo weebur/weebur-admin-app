2d3710c141c00620c4bc2b9a755b8a9f692d4c1f
elliot <elliot@wired.company>
2022-06-07 14:07:05 +0900

    fix workshop status

--- src/services/CalendarService.js
+++ src/services/CalendarService.js
@@ -6,8 +6,8 @@ export const mapStatusToBadgeColor = {
     [reservationStatus.CONFIRM_SCHEDULE.key]: theme.color.calendar.checking,
     [reservationStatus.DELIVERED_SCHEDULE.key]: theme.color.calendar.confirm,
     [reservationStatus.CONFIRM_RESERVATION.key]: theme.color.calendar.complete,
-    [reservationStatus.FINAL_CHANGE.key]: theme.color.calendar.complete,
-    [reservationStatus.FINAL_COMPLETE.key]: theme.color.calendar.complete,
+    // [reservationStatus.FINAL_CHANGE.key]: theme.color.calendar.complete,
+    // [reservationStatus.FINAL_COMPLETE.key]: theme.color.calendar.complete,
     [reservationStatus.CANCEL_RESERVATION.key]: theme.color.calendar.cancel,
     [reservationStatus.UNKNOWN.key]: theme.color.calendar.basic,
 };
