c710269d5b3109a82d33686a1d1affa077864b58
elliot <elliot@wired.company>
2022-08-19 00:39:30 +0900

    remove timeout

--- src/api/index.js
+++ src/api/index.js
@@ -4,19 +4,16 @@ const isServer = typeof window === 'undefined';
 
 const clientSideApi = axios.create({
     baseURL: '/api',
-    timeout: 5000,
     withCredentials: true,
 });
 
 const serverSideApi = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
-    timeout: 5000,
     withCredentials: true,
 });
 
 export const uploadApi = axios.create({
     baseURL: '/upload',
-    timeout: 5000,
     withCredentials: true,
 });
 
